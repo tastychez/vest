2b375fadc5719415cf1cc7a8052c78269c1450d6
Hong Zhang <hongyizhang143@gmail.com>
2026-03-29 06:16:17 -0400

    pull up resistor

--- fc22sbx.py
+++ fc22sbx.py
@@ -6,3 +6,4 @@ from config import fc22_pin
 # Configure Digital Input using pin from config.json
 fc22 = digitalio.DigitalInOut(getattr(board, fc22_pin))
 fc22.direction = digitalio.Direction.INPUT
+fc22.pull = digitalio.Pull.UP
--- flyingfish.py
+++ flyingfish.py
@@ -9,3 +9,4 @@ flyingfish = digitalio.DigitalInOut(
     getattr(board, flying_fish_pin)
 )  # Pin set via config.json
 flyingfish.direction = digitalio.Direction.INPUT
+flyingfish.pull = digitalio.Pull.UP
